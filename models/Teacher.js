2cd9694431a7d1a1629686a633ea101d5523eb0d
Alexandre <Alexandre.chekroun@etu.univ-paris1.fr>
2021-06-09 22:32:21 +0200

    premier test

--- models/Teacher.js
+++ models/Teacher.js
@@ -1,9 +1,9 @@
 class Teacher{
-    constructor(idTeacher,name) {
+    constructor(idTeacher,name, competence) {
         this._idTeacher= idTeacher;
         this._name = name;
+        this._n_competence = competence;
     }
-
 
     getIdTeacher() {
         return this._idTeacher;
@@ -13,12 +13,19 @@ class Teacher{
         this._idTeacher = value;
     }
 
-
     getName() {
         return this._name;
     }
 
     setName(value) {
         this._name = value;
+    }
+
+    getCompetence() {
+        return this._n_competence;
+    }
+
+    setCompetence(value) {
+        this._n_competence = value;
     }
 }
--- testConstraint.js
+++ testConstraint.js
@@ -46,30 +46,47 @@ S.product(["L1M","L2M","L3M","L4M","L5M","L6M"],"total");
 
 FD.distribute.naive(S, ["S1","S2","S3","S4","S5","S6","L1M","L2M","L3M","L4M","L5M","L6M"]); */
 
+let list_prof = {};
 
 
-//COMPETENCES DU PROFESSEUR1
-/* S.num("P1C1",1);
-S.num("P1C2",2); */
-S.decl("P1C",[[2,2],[4,4]]);
-//COMPETENCE DU PROFESSEUR2
-/*S.num("P2C1",3);
-S.num("P2C2",4); */
-S.decl("P2C",[[1,1],[3,3]]);
+
+//PROFS
+let t1 = new Teacher(1, "alex", 1);
+S.num("t1", t1.getIdTeacher());
+list_prof["t1"] = t1;
+
+let t2 = new Teacher(2, "david", 2);
+S.num("t2", t2.getIdTeacher());
+list_prof["t2"] = t2;
 
 // MATIERE
-S.num("M1",1);
-S.num("M2",2);
-//COMPETENCE POUR ENSEIGNER LES MATIERES 
-S.num("M1C",4);
-S.num("M2C",3);
+let c1 = new Course(1, 1, "analyse", 1);
+S.decl("c1", [[1,2]])
+let c2 = new Course(2, 2, "algèbre", 1);
+S.decl("c2", [[1,2]])
+
+//COMPETENCE POUR ENSEIGNER LES MATIERES
+/*S.num("M1C",4);
+S.num("M2C",3);*/
 
 //MATIERE ENSEIGNÉ PAR LE PROF
-S.decl("M1Prof",[[1,2]]);
-S.decl("M2Prof",[[1,2]]); 
+/*S.decl("M1Prof",[[1,2]]);
+S.decl("M2Prof",[[1,2]]);
 
-S.decl("PP",[[1,2]]);
+S.decl("PP",[[1,2]]);*/
 
+// Contraintes
+
+for(let key in list_prof){
+    if(list_prof[key].getCompetence() === c1.getType()){
+        console.log(" test " + key);
+        S.eq(key, "c1");
+    } else if(list_prof[key].getCompetence() === c2.getType()){
+        console.log(key);
+        S.eq(key, "c2");
+    }
+}
+/*
 S.eq("P1C","M1C");
 S.eq("PP","M1Prof");
 S.eq("P1C","M2C");
@@ -77,40 +94,9 @@ S.eq("PP","M2Prof");
 
 S.eq("P2C","M1C");
 
-S.eq("P2C","M2C");
+S.eq("P2C","M2C");*/
 
-FD.distribute.naive(S, ["PP","P1C","M1C","M1Prof","P2C","M2C","M2Prof"]);
-
-//Ensemble des profs
-S.decl("PP",[[1,2]]);
-//Competence de chaque prof
-S.decl("P1C",[[2,2],[4,4]]);
-S.decl("P2C",[[1,1],[3,3]]);
-
-// MATIERE
-S.num("M1",1);
-S.num("M2",2);
-//COMPETENCE POUR ENSEIGNER LES MATIERES 
-S.num("M1C",4);
-S.num("M2C",3);
-
-//MATIERE ENSEIGNÉ PAR LE PROF
-S.decl("M1Prof",[[1,2]]);
-S.decl("M2Prof",[[1,2]]); 
-
-
-
-S.eq("P1C","M1C");
-S.eq("PP","M1Prof");
-S.eq("P1C","M2C");
-S.eq("PP","M2Prof");
-
-S.eq("P2C","M1C");
-
-S.eq("P2C","M2C");
-
-FD.distribute.naive(S, ["PP","P1C","M1C","M1Prof","P2C","M2C","M2Prof"]);
-
+FD.distribute.naive(S, ["c1", "t1", "t2"]);
 
 
 
